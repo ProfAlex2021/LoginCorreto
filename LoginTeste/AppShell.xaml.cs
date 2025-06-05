f8d38b52ba0ae23beef638f983a0b6f2f6aa4baf
Professor DS <professor@ds.etec>
2025-06-04 21:58:54 -0300

    Add project files.

---
+++ LoginTeste/AppShell.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace LoginTeste
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute("Main", typeof(MainPage));
+            Routing.RegisterRoute("Login", typeof(LoginPage));
+
+            string usuario = Preferences.Default.Get("usuario", "");
+
+            if(!usuario.Equals(String.Empty))
+                Shell.Current.GoToAsync("Main");
+        }
+    }
+}
---
+++ LoginTeste/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace LoginTeste
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnLogout.Text += " - " + Preferences.Default.Get("usuario", "");
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private async void btnLogout_Clicked(object sender, EventArgs e)
+        {
+            Preferences.Default.Remove("usuario");
+
+            await Shell.Current.GoToAsync("Login");
+        }
+        protected override bool OnBackButtonPressed() { return true; }
+    }
+
+}
